c98d6b89b81a2a7a14d7584ba27d46d81d90478b
DoubleRiichi <DoubleRiichi>
2025-03-20 14:47:35 +0100

    added routes

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,29 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post("/users", (req, res) => {
+  const {name, email} = req.body;
+  res.json({message : 'Utilisateur crée avec succès', user : {name, email}});
+});
+
+router.get("/users/:id", (req, res) =>  {
+});
+
+routeur.put("/users/:id", (req, res) => {
+  const {id} = req.params;
+  const {name, email} = res.body;
+  res.json({
+    message: 'Utilisateur mis à jour avec succès!',
+    user: {
+      id, name, email
+    }
+  });
+});
+
+app.delete('/users/:id', (req, res) => {
+  const { id } = req.params;
+  // Supprimer l'utilisateur avec l'ID spécifié
+  res.json({ message: 'Utilisateur supprimé avec succès', user: { id } });
+});
+
 module.exports = router;
